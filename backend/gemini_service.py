b83f50f2f3c7963a4ef1b23a3c39feae80f0ba2c
RomanovYehor <yeghor.romanov@gmail.com>
2025-12-04 19:44:05 +0200

    clear merge conflicts

--- backend/gemini_service.py
+++ backend/gemini_service.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from abc import ABC, abstractmethod
 
 from backend.types import TaroTypes
@@ -11,7 +10,3 @@ class AIService(ABC):
 class GeminiService(AIService):
     def get_prediction_desc(predict_type, prompt, cards):
         pass
-=======
-class GeminiService: # прописать какие-то методы 
-    pass
->>>>>>> f1cbb1c1014ac9d2e660c3f65ebb29167d8144fc
--- backend/pydantic_models.py
+++ backend/pydantic_models.py
@@ -4,10 +4,6 @@ from typing import List, Any
 
 #сделать тип для карточки (название, описание)
 # DTOs
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f1cbb1c1014ac9d2e660c3f65ebb29167d8144fc
 
 class Card(BaseModel):
     name: str
@@ -20,11 +16,3 @@ class PredictionResponse(BaseModel):
     prediction: str 
     cards: List[CardExtended]
     image_name: str 
-
-<<<<<<< HEAD
-=======
-=======
-class Schema:
-    pass
->>>>>>> c8bad3ac930c8886d7b7f5b50dd123ac32225848
->>>>>>> f1cbb1c1014ac9d2e660c3f65ebb29167d8144fc
