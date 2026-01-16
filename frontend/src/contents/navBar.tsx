4719df009ca10f647977ae0b38630a0fc6f172fd
RomanovYehor <yeghor.romanov@gmail.com>
2026-01-16 16:26:05 +0200

    fix revert commit

--- frontend/src/contents/navBar.tsx
+++ frontend/src/contents/navBar.tsx
@@ -55,33 +55,8 @@ const NavbarComp = () => {
         >
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full relative">
                 <div className="flex items-center justify-between h-20">
-                    
                     <div className="flex items-center gap-3 z-10">
-                        <button 
-                            onClick={toggleTheme}
-                            className="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none transition-all duration-300 active:shadow-[0_0_15px_rgba(168,85,247,0.8)] active:text-purple-500 active:scale-95"
-                            aria-label="Toggle Theme"
-                        >
-                        {theme === 'light' ? (
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-yellow-500 hover:text-inherit active:text-inherit">
-                                <circle cx="12" cy="12" r="5"></circle>
-                                <line x1="12" y1="1" x2="12" y2="3"></line>
-                                <line x1="12" y1="21" x2="12" y2="23"></line>
-                                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-                                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-                                <line x1="1" y1="12" x2="3" y2="12"></line>
-                                <line x1="21" y1="12" x2="23" y2="12"></line>
-                                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-                                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-                            </svg>
-                        ) : (
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-blue-300 hover:text-inherit active:text-inherit">
-                                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                            </svg>
-                        )}
-                        </button>
-                        
-                        <Link to="/" className="text-2xl font-bold bg-gradient-to-r from-purple-600 to-blue-500 bg-clip-text text-transparent cursor-pointer tracking-wide hover:opacity-80 transition-opacity">
+                        <Link to="/" className="text-2xl font-bold bg-linear-to-r from-purple-600 to-blue-500 bg-clip-text text-transparent cursor-pointer tracking-wide hover:opacity-80 transition-opacity">
                             Taro 
                         </Link>
                     </div>
